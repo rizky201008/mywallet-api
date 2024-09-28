2605d995529bc2dfdc66d2f51eeab621e7c647e9
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-09-29 03:22:30 +0700

    feat: create error handler and custom error

---
+++ exception/error_handler.go
@@ -0,0 +1,36 @@
+package exception
+
+import (
+	"errors"
+	"github.com/gofiber/fiber/v2"
+)
+
+func ErrorHandler(ctx *fiber.Ctx, err error) error {
+	notFoundErr, notFound := notFoundError(ctx, err)
+	if notFound {
+		return notFoundErr
+	}
+
+	ctx.Status(fiber.StatusInternalServerError)
+	return ctx.JSON(fiber.Map{
+		"status":  "1",
+		"message": err.Error(),
+		"data":    nil,
+	})
+}
+
+func notFoundError(ctx *fiber.Ctx, err error) (error, bool) {
+	var exception NotFoundError
+	ok := errors.As(err, &exception)
+	if ok {
+		ctx.Status(fiber.StatusNotFound)
+		return ctx.JSON(fiber.Map{
+			"status":  "2",
+			"message": exception.Error(),
+			"data":    nil,
+		}), true
+
+	} else {
+		return nil, false
+	}
+}
