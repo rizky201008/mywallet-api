d066be2a79f8d5b2052419e382d67932e8f960b2
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-09-29 00:24:40 +0700

    feat: add transactionController on InitRouter parameter

--- app/fiber.go
+++ app/fiber.go
@@ -2,17 +2,18 @@ package app
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/rizky201008/mywallet-backend/controller"
 	"github.com/spf13/viper"
 )
 
-func InitFiber(viper *viper.Viper) {
+func InitFiber(viper *viper.Viper, transactionController controller.TransactionController) {
 	app := fiber.New()
 
 	app.Get("/", func(ctx *fiber.Ctx) error {
 		return ctx.Redirect("/api")
 	})
 
-	InitRouter(app)
+	InitRouter(app, transactionController)
 
 	err := app.Listen(viper.GetString("app.APP_PORT"))
 
