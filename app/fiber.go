bd3b1a08adea56158fb6340401145100b605b958
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-09-29 03:23:02 +0700

    refactor: add ErrorHandler config

--- app/fiber.go
+++ app/fiber.go
@@ -2,12 +2,18 @@ package app
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/rizky201008/mywallet-backend/controller"
+	"github.com/rizky201008/mywallet-backend/exception"
 	"github.com/spf13/viper"
 )
 
 func InitFiber(viper *viper.Viper, transactionController controller.TransactionController) {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		ErrorHandler: exception.ErrorHandler,
+	})
+
+	app.Use(recover.New())
 
 	app.Get("/", func(ctx *fiber.Ctx) error {
 		return ctx.Redirect("/api")
