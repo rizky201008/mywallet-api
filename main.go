6cc96cb3a26e7833ece3c11d73f15b816731eaf8
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-09-29 00:25:21 +0700

    feat: initialize transactionRepo, transactionService, transactionController

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
-import "github.com/rizky201008/mywallet-backend/app"
+import (
+	"github.com/rizky201008/mywallet-backend/app"
+	"github.com/rizky201008/mywallet-backend/controller"
+	"github.com/rizky201008/mywallet-backend/repository"
+	"github.com/rizky201008/mywallet-backend/service"
+)
 
 func main() {
 	viper := app.InitViper()
-	app.InitDb(viper)
-	app.InitFiber(viper)
+	db := app.InitDb(viper)
+	transactionRepo := repository.NewTransactionRepository()
+	transactionService := service.NewTransactionService(db, transactionRepo)
+	transactionController := controller.NewTransactionController(transactionService)
+	app.InitFiber(viper, transactionController)
 }
