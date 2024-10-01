617f577fe0f96540e24aaca1239388e0b6f691d1
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-10-01 19:49:30 +0700

    refactor: modify CreateUser method and create Login method

---
+++ model/web/response_login.go
@@ -0,0 +1,5 @@
+package web
+
+type ResponseLogin struct {
+	Token string `json:"token"`
+}
--- service/user_service.go
+++ service/user_service.go
@@ -2,12 +2,16 @@ package service
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt"
 	"github.com/rizky201008/mywallet-backend/exception"
 	"github.com/rizky201008/mywallet-backend/helper"
 	"github.com/rizky201008/mywallet-backend/model/web"
 	"github.com/rizky201008/mywallet-backend/repository"
+	"github.com/spf13/viper"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 	"strconv"
+	"time"
 )
 
 type UserService interface {
@@ -15,15 +19,52 @@ type UserService interface {
 	UpdateUser(ctx *fiber.Ctx) web.ResponseUser
 	FindUserById(ctx *fiber.Ctx) web.ResponseUser
 	DeleteUser(ctx *fiber.Ctx)
+	Login(ctx *fiber.Ctx) web.ResponseLogin
 }
 
 type UserServiceImpl struct {
 	UserRepo repository.UserRepository
 	DB       *gorm.DB
+	Viper    *viper.Viper
 }
 
 func NewUserService(userRepo repository.UserRepository, db *gorm.DB) UserService {
 	return UserServiceImpl{UserRepo: userRepo, DB: db}
+}
+
+func (service UserServiceImpl) Login(ctx *fiber.Ctx) web.ResponseLogin {
+	p := new(web.RequestUser)
+	err := ctx.BodyParser(p)
+	if err != nil {
+		panic(err)
+	}
+	foundedUser, err := service.UserRepo.FindUserByUsername(service.DB, p.Username)
+	if err != nil {
+		panic(exception.NotFoundError{
+			Err: err.Error(),
+		})
+	}
+
+	notMatch := bcrypt.CompareHashAndPassword([]byte(foundedUser.Password), []byte(p.Password))
+	if notMatch != nil {
+		panic(exception.NotMatchError{
+			Err: notMatch.Error(),
+		})
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": foundedUser.ID,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	})
+	secret := service.Viper.GetString("secrets.JWT_SECRET")
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte(secret))
+	if err != nil {
+		panic(err)
+	}
+	return web.ResponseLogin{
+		Token: tokenString,
+	}
 }
 
 func (service UserServiceImpl) CreateUser(ctx *fiber.Ctx) web.ResponseUser {
@@ -32,6 +73,11 @@ func (service UserServiceImpl) CreateUser(ctx *fiber.Ctx) web.ResponseUser {
 	if err != nil {
 		panic(err)
 	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(p.Password), bcrypt.DefaultCost)
+	if err != nil {
+		panic(err)
+	}
+	p.Password = string(hashedPassword)
 	var response web.ResponseUser
 	err = service.DB.Transaction(func(tx *gorm.DB) error {
 		user, err := service.UserRepo.CreateUser(service.DB, helper.RequestUserToUser(*p))
